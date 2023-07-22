ed8b81c5000afc6cd1bf78cf0e4f9e498177bee1
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-07-22 19:50:55 +0300

    Prime Numbers

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,8 +1,12 @@
 def is_prime(number):
-    if number > 1:
-        for i in range(2, int(number/2)+1):
+
+    if number == 1:
+        return False
+    elif number > 1:
+        for i in range(2, number):
             if (number % i) == 0:
                 return False
+                break
             else:
                 return True
     else:
