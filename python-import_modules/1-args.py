94b48138a164371183e8f9f9170d522675c65e57
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-07-25 16:07:47 +0300

    dynamic script

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    a = len(sys.argv)
+
+    if a == 1:
+        print(0, "arguments.")
+    elif a == 2:
+        print(a-1, "argument:")
+        for i in range(1, len(sys.argv)):
+            print(i, ":", sys.argv[1])
+
+    else:
+        print(a - 1, " arguments:")
+        for i in range(1, len(sys.argv)):
+            print(i, ":", sys.argv[i])
