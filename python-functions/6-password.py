88a9b977790c5c7a547e804f96ca61a7e0c448c9
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-07-28 23:08:08 +0300

    Password

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,16 +1,23 @@
 def validate_password(password):
-    if len(password) > 8:
-        lower = upper = digit = special = False
-
-        for i in password:
-            if i.islower():
-                lower = True
-            if i.isupper():
-                upper = True
-            if i.isdigit():
-                digit = True
-            if i.isalnum():
-                special = True
-        return lower and upper and digit and special
-    else:
+    # Check if the password is at least 8 characters long
+    if len(password) < 8:
         return False
+        # Check if the password contains at least one uppercase letter, one lowercase letter, and one digit
+    lower = upper = digit = False
+
+    for i in password:
+        if i.islower():
+            lower = True
+        if i.isupper():
+            upper = True
+        if i.isdigit():
+            digit = True
+        # If all required character types are found, break the loop to save unnecessary iterations
+        if lower and upper and digit:
+            break
+    if not (lower and upper and digit):
+        return False
+        # Check if the password contains spaces
+    if " " in password:
+        return False
+    return True
