64163e46c56f9a0a6bdcb2b0b3271165e4896f52
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-03 11:25:20 +0300

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,10 @@
+"""This module returns the size of a square """
+
+
 class Square:
     """class for defining a square size """
     def __init__(self, size):
         """ instantiation with size variable"""
+
         self.__size = size
         """private instance attribute"""
