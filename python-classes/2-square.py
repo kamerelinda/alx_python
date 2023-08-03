8d8e293b8ba34803144c3341a96013e70db51693
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-03 11:50:20 +0300

    Area of a square

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+""" This module defines a square"""
+
+
+class Square:
+    """ class that defines a square"""
+    def __init__(self, size=0):
+        """Instantiate with size equal to 0"""
+        self.__size = size
+        """private instance attribute"""
+        if type(size) != int:
+            """if the size is not an integer"""
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            """if size is less than 0"""
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """public instance method called area"""
+        return self.__size * self.__size
+    """returns the square of size"""
