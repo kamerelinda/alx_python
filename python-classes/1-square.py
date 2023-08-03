3b319bfe63931bb8871e9b5576650fe085a71498
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-03 11:38:55 +0300

    size validation

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+""" This module defines a square"""
+
+
+class Square:
+    """ class that defines a square"""
+    def __init__(self, size=0):
+        """Instantiate with size equal to 0"""
+        self.__size = size
+        """private instance attribute"""
+        if type(size) != int:
+            """if the size is not an integer"""
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            """if size is less than 0"""
+            raise ValueError("size must be >= 0")
