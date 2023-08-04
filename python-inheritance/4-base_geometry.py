5e785afad6c80a2a6d5bf5211eb528ac2a70c2d6
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-04 22:35:31 +0300

    Improve Geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -3,6 +3,17 @@
 
 class BaseGeometry:
     """This class defines a base geometry"""
+    def __dir__(cls) -> None:
+        """Magic method that allows you to override default dir"""
+        attributes = super().__dir__()
+        """get list of all attributes for this class and exclude __init_subclass"""
+        list_to_return = []
+        for attr in attributes:
+            if attr != '__init_subclass__':
+                """append this to the list_to_return"""
+                list_to_return.append(attr)
+        return list_to_return
+
     def area(self):
         """Public instance method that raises an exception method"""
         raise Exception("area() is not implemented")
