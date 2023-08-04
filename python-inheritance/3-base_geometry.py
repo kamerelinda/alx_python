9adc13f1578e92748267d6f89f2ca68af6f016f0
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-04 22:12:16 +0300

    Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -3,3 +3,10 @@
 
 class BaseGeometry:
     """This class passes nothing"""
+
+
+def __dir__(cls):
+    """Magic method that allows you to override default dir"""
+    attributes = super().__dir__()
+    """get list of all attributes for this class and exclude __init_subclass"""
+    return [attribute for attribute in attributes if attribute != '__init_subclass__']
