83302f3532cd1e609998530224a60c6bdd5c0b51
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-08 15:45:04 +0300

    Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,16 +1,17 @@
 """This module has an empty class"""
 
 
-class BaseGeometry:
+class MetaGeometry(type):
+    """this class overrides the dir init subclass in the class"""
+
+    def __dir__(cls):
+        """Magic method that allows you to override default dir"""
+        return (attribute for attribute in super().__dir__() if attribute != '__init_subclass__')
+
+
+class BaseGeometry(metaclass=MetaGeometry):
     """This class passes nothing"""
 
-    def __dir__(cls) -> None:
+    def __dir__(cls):
         """Magic method that allows you to override default dir"""
-        attributes = super().__dir__()
-        """get list of all attributes for this class and exclude __init_subclass"""
-        list_to_return = []
-        for attr in attributes:
-            if attr != '__init_subclass__':
-                """append this to the list_to_return"""
-                list_to_return.append(attr)
-        return list_to_return
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
