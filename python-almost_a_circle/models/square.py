91f52a53b0d63e34573f0c95e8e8035004a1c0b8
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-09 22:22:19 +0300

    Square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,11 @@
+from models.rectangle import Rectangle
+"""This module imports methods and arguments from rectangle"""
+
+
+class Square(Rectangle):
+    """This class forms a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
