a7c1ae7296de4d34307167e3d6611b4afe5c75ca
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-09 23:52:28 +0300

    response header value

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""
+Fetches a URL and prints the X-Request-Id header value.
+Usage: ./fetch_request_id.py <URL>
+"""
+
 import requests
 import sys
 
-"""takes in url as the first argument """
 r = sys.argv[1]
 """sends a request to the url"""
 response = requests.get(r)
