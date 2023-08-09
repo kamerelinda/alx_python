153a5f7cf75b6a52f67cb27d3607d1e5f2047405
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-10 01:30:26 +0300

    Error Code

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -5,10 +5,16 @@ sends a request to the URL and displays the body of the response.
 import requests
 import sys
 
-url = sys.argv[1]
 
-response = requests.get(url)
-if response.status_code >= 400:
-    print("Error code:", response.status_code)
-else:
-    print("{}", response.text)
+def main():
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print("{}", response.text)
+
+
+if __name__ == "__main__":
+    main()
