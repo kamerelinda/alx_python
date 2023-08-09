b283a606f281e62963640f0d5b6694eaa65edcef
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-09 20:17:00 +0300

    display 1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -76,7 +76,7 @@ class Rectangle(Base):
         for j in range(self.__y):
             print(" ")
         for i in range(self.__height):
-            print(" "*self.__x, "#"*self.__width)
+            print(""*self.__x, "#"*self.__width)
 
     def __str__(self):
         """str function returns a string"""
