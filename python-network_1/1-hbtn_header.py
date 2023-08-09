f2159a2ad0ea2980aae05fdb6f49a105d08da869
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-09 23:33:52 +0300

    response header value

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,7 +2,8 @@
 """This script uses request and sys"""
 import requests
 import sys
-
+"""Takes in a URL, sends a request to the URL and displays 
+the value of the variable X-Request-Id in the response header"""
 r = sys.argv[1]
 response = requests.get(r)
 print(response.headers['X-Request-Id'])
