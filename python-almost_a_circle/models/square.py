4c7de044cd38437b0d163aae5668eaa944c04b47
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-09 22:32:00 +0300

    Square size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,7 +5,21 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """This class forms a square"""
     def __init__(self, size, x=0, y=0, id=None):
+        """instantiate"""
         super().__init__(size, size, x, y, id)
+        """calling the super method in order"""
 
     def __str__(self):
+        """returns a string"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """getter function of retrieving size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter method"""
+        self.width = value
+        self.height = value
