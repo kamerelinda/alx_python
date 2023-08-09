2f5c6fc089e8137cd72423ed50f21e7b1a1f3934
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-09 16:43:12 +0300

    Base Class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+"""This module creates the first class which is a base class"""
+
+
+class Base:
+    """class called Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
