9b7f51794795e6813ddce9c6b840c66def6dc714
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-09 23:14:44 +0300

    status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """imports the request package"""
 import requests
+
 r = requests.get('https://alu-intranet.hbtn.io/status')
 print("Body response:")
 
-print("\t- type: ", str(type(r)))
-print("\t- content: ", r.content)
+print("\t- type: ", (type(r.text)))
+print("\t- content: ", r.text)
