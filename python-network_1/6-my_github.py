3ebf8f6b25291ae4ed6192f8e633edcfd6d0415e
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-11 16:49:55 +0300

    My Github

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This script takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+import requests
+import sys
+
+
+def main():
+    username = sys.argv[1]
+    pat_password = sys.argv[2]
+
+    response = requests.get('https://api.github.com/user', auth=(username, pat_password))
+
+    if response.status_code == 200:
+        data = response.json()
+        if id in data:
+            print(data['id'])
+        else:
+            print(None)
+    else:
+        print(response.text)
+
+
+if __name__ == "__main__":
+    main()
