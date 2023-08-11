12818ef8c6da326cf07ec0ecfc88df83fedd37b5
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-11 13:57:51 +0300

    Full Rectangle

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,16 +1,9 @@
 """This module improves the class by raising an exception"""
-
 BaseGeometry = __import__('6-rectangle').Rectangle
 
 
 class Rectangle(BaseGeometry):
     """class that inherits BaseGeometry"""
-
-    def __init__(self, width, height):
-        """instantiation with width and height"""
-        self.__width = super().integer_validator('width', width)
-        self.__height = super().integer_validator('height', height)
-
     def area(self):
         """calculate and return the area of rectangle"""
         return self.__width * self.__height
