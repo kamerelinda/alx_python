4f6d8261be217c78eab63413eb8b93ee36174672
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-11 12:41:37 +0300

    Rectangle

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,28 +1,6 @@
 """This module improves the class by raising an exception"""
 
-
-class MetaGeometry(type):
-    """this class overrides the dir init subclass in the class"""
-
-    def __dir__(cls):
-        """Magic method that allows you to override default dir"""
-        return (attribute for attribute in super().__dir__() if attribute != '__init_subclass__')
-
-
-class BaseGeometry(metaclass=MetaGeometry):
-    """This class defines a base geometry"""
-
-    def area(self):
-        """Public instance method that raises an exception"""
-        raise Exception("area() is not implemented")
-
-    @staticmethod
-    def integer_validator(name, value):
-        """public instance method that validates value"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('6-rectangle').Rectangle
 
 
 class Rectangle(BaseGeometry):
