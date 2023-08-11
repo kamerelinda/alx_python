c3be5ddbbee113889a32a9eeb3ea7ceda9212316
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-11 12:38:28 +0300

    square

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,50 +1,6 @@
 """This module improves the class by raising an exception"""
 
-
-class MetaGeometry(type):
-    """this class overrides the dir init subclass in the class"""
-
-    def __dir__(cls):
-        """Magic method that allows you to override default dir"""
-        return (attribute for attribute in super().__dir__() if
-                attribute != '__init_subclass__')
-
-
-class BaseGeometry(metaclass=MetaGeometry):
-    """This class defines a base geometry"""
-
-    def area(self):
-        """Public instance method that raises an exception"""
-        raise Exception("area() is not implemented")
-
-    @staticmethod
-    def integer_validator(name, value):
-        """public instance method that validates value"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """class that inherits BaseGeometry"""
-
-    def __init__(self, width, height):
-        """instantiation with width and height"""
-        self.__width = width
-        self.__height = height
-        """Height and width are private"""
-        self.integer_validator('width', self.__width)
-        self.integer_validator('height', self.__height)
-        """using the integer validator method for our variables"""
-
-    def area(self):
-        """calculate and return the area of rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """method returns a string"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+Rectangle = __import__('7-rectangle').Rectangle
 
 
 class Square(Rectangle):
