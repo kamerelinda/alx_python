ced17cdfe22d280bee1a45527fb8f2971442d135
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-23 15:43:51 +0300

    Filter states by input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# This script filters all states where name matches argument
+# imports module MySQLdb
+import MySQLdb
+import sys
+
+
+def main():
+    database_name = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name_searched = sys.argv[4]
+
+    # Connecting to database in the localhost
+    database = MySQLdb.connect(host='localhost', user=username,
+                               passwd=password, db=database_name,
+                               port=3306)
+
+    # create a cursor
+    cur = database.cursor()
+
+    # finding all the states in the database beginning with N
+    cur.execute("SELECT * FROM states "
+                "WHERE name = '{}' "
+                "ORDER BY id ASC".format(name_searched))
+
+    # obtaining the results
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # close cursor
+    cur.close()
+
+    # close database
+    database.close()
+
+
+if __name__ == "__main__":
+    main()
