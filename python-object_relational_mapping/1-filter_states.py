1101af49a6cca693d7eb2a15d05136a0561bfbe3
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-23 15:02:59 +0300

    Get all states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,17 +6,20 @@ import sys
 
 def main():
     database_name = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
 
     # Connecting to database in the localhost
-    database = MySQLdb.connect(host='localhost', user='root',
-                               passwd='fabi2star.', db=database_name,
+    database = MySQLdb.connect(host='localhost', user=username,
+                               passwd=password, db=database_name,
                                port=3306)
 
     # create a cursor
     cur = database.cursor()
 
     # finding all the states in the database
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC ")
+    cur.execute("SELECT * FROM states "
+                "WHERE name LIKE 'N%' ORDER BY states.id ASC ")
 
     # obtaining the results
     rows = cur.fetchall()
