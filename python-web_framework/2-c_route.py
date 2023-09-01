556585758ffb4bac1ddbcce73e6a207f852fa6c3
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-09-01 16:56:23 +0300

    dynamic routing

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,31 @@
+""" This module creates a web application with a routing to hbnb """
+from flask import Flask
+
+
+app = Flask(__name__)
+""" creates a flask web application """
+
+
+@app.route('/', strict_slashes=False)
+def greetings():
+    """ define the route for the root url with strict slashes false """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ define the route for the hbnb url with strict slashes false """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def disp_text(text):
+    """ display “C ” followed by the value of the text variable
+     and replaces _ with space """
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+if __name__ == '__main__':
+    # run flask app on 0.0.0.0 and port 5000
+    app.run(debug=True, host='0.0.0.0', port=5000)
